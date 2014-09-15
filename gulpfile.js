23f344168eb6e906a6b08be5634f96b4ded1b4ee
atamon <lindgren.anton@gmail.com>
2014-09-15 16:59:46 +0200

    First draft of event intro text.

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    watch = require('gulp-watch'),
+    minify = require('gulp-minify-css');
+
+var buildSass = function() {
+  gulp.src('stylesheets/*.scss')
+      .pipe(sass({
+        outputStyle: 'compressed'
+      }))
+      .pipe(minify({ cache: true }))
+      .pipe(gulp.dest('./css'));
+};
+
+gulp.task('default', ['styles']);
+
+gulp.task('styles', buildSass);
+
+gulp.task('watch', function() {
+  gulp.src('stylesheets/*.scss')
+      .pipe(watch('*'))
+      .pipe(sass({
+        errLogToConsole: true
+      }))
+      .pipe(gulp.dest('./css'));
+});
